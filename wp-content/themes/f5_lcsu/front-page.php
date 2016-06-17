0757815c43270a55a18fd5c7b1f4650e25de4838
rob orr <rob@sachsmedia.com>
2016-06-17 16:54:32 -0400

    fixes

--- wp-content/themes/f5_lcsu/front-page.php
+++ wp-content/themes/f5_lcsu/front-page.php
@@ -15,7 +15,7 @@ get_header(); ?>
                 </div>
                 <div class="row text-center">
                     <div class="small-12 columns pjm-hero-inner">
-                        <h4 class="hero-more">LSCU & CUNA provide member credit unions visible action on the local, state and national stages.
+                        <h4 class="hero-more">LSCU & CUNA provide member credit unions visible action on the local, state & national stages.
                         </h4>
                         </h4>
                         <p class="pjm-button">
@@ -24,9 +24,15 @@ get_header(); ?>
                     </div>
                 </div>
                 <div class="row text-center">
+                <div class="small-12 columns">
+                        <p><strong>The unique partnership between CUNA & LSCU generates:</strong></p>
+
+                </div>
+                </div>
+                <div class="row text-center">
                     <div class="medium-4 small-12 columns">
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/lscu-strong-01.svg" />
-                        <p>Strong, coordinated Federal &amp; State advocacy efforts</p>
+                        <p>Strong, coordinated federal &amp; state advocacy efforts</p>
                     </div>
                     <div class="medium-4 small-12 columns">
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/lscu-realtime-02.svg" />
@@ -38,7 +44,7 @@ get_header(); ?>
                     </div>
                     <div class="row">
                         <div class="small-12 columns">
-                    <p><strong>... and so much more. Together, LSCU & CUNA create real results for our member credit unions</strong></p>
+                    <p><strong>... and so much more. Together, LSCU & CUNA create real results for our member credit unions.</strong></p>
                     </div>
                     </div>
                 </div>
@@ -64,21 +70,6 @@ get_header(); ?>
     <div class="row">
         <div class="small-12 columns v-align">
             <div class="v-align-wrap">
-                <h3 class="pjm-about">
-                The unique national-local partnership between CUNA & LSCU generates:
-                </h3>
-                <ul>
-                            <li>Strong, coordinate Fedearl & state advocacy efforts</li>
-                            <li>Real-time compliance help from experts</li>
-                            <li>Public-facing efforts that grow credit unions</li>
-                            <li>...and so much more. Together LSCU & CUNA create real results for our member credit unions.</li>
-                        </ul>
-                <p>Our member credit unions prosper as CUNA and LSCU work hand-in-hand to deliver the best results in credit union legislative advocacy, compliance assistance, and public-facing campaigns.</p>
-
-            <p>Action across the local LSCU network leads to bigger, better impact at home and nationwide.</p>
-            <p>Our member credit unions prosper as CUNA and LSCU work hand-in-hand to deliver the best results in credit union legislative advocacy, compliance assistance, public-facing campaigns, training and professional development. </p>
- 
-            <p>Action across the local LSCU network leads to bigger, better impact at home and nationwide.</p>
             
             </div>
         </div>
@@ -86,30 +77,49 @@ get_header(); ?>
 </div>
 <div class="full-width about-two notched-box">
     <div class="ps-border ps-translucent"></div>
+    
     <div class="row text">
-        <div class="small-12 columns">
-            <p align="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webgraphic-1.png" / ></p>                        
 
+        <div class="small-12 columns">
+                                   
+             <p class="text-center"><strong>Our member credit unions prosper as CUNA and LSCU work hand-in-hand to deliver the best results in credit union legislative advocacy, compliance assistance, public-facing campaigns, training and professional development.</strong> </p>
+ 
+            <p class="text-center"><strong>Action across the local LSCU network leads to bigger, better impact at home and nationwide.</strong></p>
             <h2 class="text-center">Strong, coordinated federal & state advocacy.</h2>
             <p>With partner state leagues across the United States, CUNA is able to advocate on Capitol Hill for all credit unions with the best insight and access of any trade association. With an equipped, empowered, and organized army of credit union leagues and tens of thousands of grassroots advocates, CUNA is the most influential credit union trade association in Washington.</p>
              
             <p>Member credit unions in Florida and Alabama gain access to state and federal decision-makers through LSCU, influencing regulations and legislation at both the national level and state level, too. Member-exclusive newsletters, action alerts and more keep credit union leaders up to date on the latest and most-impactful regulatory and legislative issues.</p>
-            <p align="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webgraphic-2.png" / ></p>
+            
             <h2 class="text-center">Real-time regulatory compliance help from experts.</h2>
-            <p>At the state level, LSCU daily empowers its member credit unions to grow and succeed. There’s no waiting game when you need customized compliance answers fast; LSCU and CUNA’s resources are second to none. LSCU members have access to responsive, dedicated, and well-known compliance experts Bill Berg and Mike Lee -- only a quick text, phone call or email away.
+            <p>At the state level, LSCU daily empowers its member credit unions to grow and succeed. There’s no waiting game when you need customized compliance answers fast; LSCU and CUNA’s resources are second to none. LSCU members have access to responsive, dedicated, and well-known compliance experts Bill Berg and Mike Lee &mdash; only a quick text, phone call or email away.
  
             <p>Forward-thinking and innovative, LSCU has tapped into CUNA’s robust network of state and regional leagues to develop an online compliance network, TotalCompliance, boasting shared resources that work to the advantage of credit union leaders.</p>
 
             <p>CUNA’s extensive federal compliance resources, including the CUNA eGuide to Federal Laws & Regulations and its members-only Compliance Community provides credit unions the most comprehensive federal tools, networking and information.</p>
-            <p align="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webgraphic-3.png" / ></p>
+           
             <h2 class="text-center">Championing the cause of credit unions.</h2>
             <p>Our public-facing campaigns generate interest in credit unions and lead to growth for our members. That, combined with winning media outreach, gets our member credit unions’ message seen and heard in the media and among potential members.</p>
  
             <p>Campaigns such as LSCU’s Cooperative Image and CUNA’s Don’t Tax My Credit Union, Stop the Data Breaches, and Ease the Burden have track records of success, effectively communicating the credit union brand proposition to consumers to strengthen relationships with members and establish new relationships with potential members. CUNA and LSCU provide top messaging and communication tools so that our member credit unions can achieve the results they need regardless of time, budget and resource constraints.</p>
 
-            
 
+
+            <h2 class="text-center">When you become a member credit union you gain unparalleled access to:</h2>
+            <ul>
+                <li>state and federal lawmakers</li>
+                <li>strong advocacy at the federal and state level</li>
+                <li>action alerts and breaking news updates on regulatory and legislative issues</li>
+                <li>real-time custom compliance help, resources and model policies from experts you know and trust</li>
+                <li>communications tools and advertising campaigns that show the credit union difference</li>
+                <li>and so much more.</li>
+            </ul>
+
+
+            <p align="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webgraphic-1.png" / ></p> 
+            <p align="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webgraphic-2.png" / ></p>
+             <p align="center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/webgraphic-3.png" / ></p>
         </div>
+
     </div>
 </div>
 <div class="full-width testimonials notched-box">
@@ -182,7 +192,7 @@ get_header(); ?>
         <div class="small-12 medium-9 columns small-only-text-center">
             <p>“I would tell any credit union official that if you have the resources and the desire to belong to other associations outside the League/CUNA structure, then go ahead. However, to make the choice to support only those organizations at the exclusion of the LSCU and CUNA is shortsighted. A weaker CUNA/League system leaves us more exposed to the many environmental threats so evident today. I believe our very survival depends on a strong CUNA and a responsive League.”</p>
             <p>
-                <blockquote>-Steve Swofford, President/CEO, Alabama CU, Tuscaloosa, AL</blockquote>
+                <blockquote>Steve Swofford, President/CEO, Alabama CU, Tuscaloosa, AL</blockquote>
             </p>
         </div>
     </div>
@@ -209,3 +219,4 @@ get_header(); ?>
     <!--  -->
 </div>
 <?php get_footer(); ?>
+   
