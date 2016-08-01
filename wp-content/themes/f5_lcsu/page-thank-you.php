83476650999fd326c6ee530e10a2cf38c499e665
rob orr <rob@sachsmedia.com>
2016-08-01 14:33:03 -0400

    adding thank you page

---
+++ wp-content/themes/f5_lcsu/page-thank-you.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Thank You
+*/
+get_header(); ?>
+<header class="pjm-hero" role="banner">
+    <div class="full-width pjm-hero notched-box">
+        <div class="ps-border ps-translucent"></div>
+        <div class="row">
+            <div class="small-12 columns text-center">
+                <div class="row text-center">
+                    <div class="small-12 columns pjm-hero-inner start">
+                        <h2 class="hero-title">Thank you.</h2>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div class="small-12 columns pjm-hero-inner">
+                        <h4 class="hero-more">We will follow up with you shortly.
+                        </h4>
+<!--                         <p class="pjm-button">
+                            <a href="#join" class="button">Yes! I'm ready to join!</a>
+                        </p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+
+<?php get_footer(); ?>
+   
